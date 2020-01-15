c87f9236cbb0df4df04e6550f49f20f2cc974640
jonathanpan777 <jonathanpan@berkeley.edu>
2020-01-14 23:40:16 -0800

    jennie

--- script.js
+++ script.js
@@ -5,14 +5,20 @@
 var button = document.getElementById("button");
 var draft = document.getElementById("draft");
 var notdraft = document.getElementById("notdraft");
+var happyjennie = document.getElementById("happyjennie");
+var sadjennie = document.getElementById("sadjennie");
 
 
 button.onclick = function() {
     if (Math.random() <= 0.2) {
         draft.style.display = 'block'
+        happyjennie.style.display = 'block'
         notdraft.style.display = 'none'
+        sadjennie.style.display = 'none'
     } else {
         notdraft.style.display = 'block'
+        sadjennie.style.display = 'block'
         draft.style.display = 'none'
+        happyjennie.style.display = 'none'
     }
 };
