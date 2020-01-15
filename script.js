5cef46f397153a4dc89ced14ca7bf178c11e0888
jonathanpan777 <jonathanpan@berkeley.edu>
2020-01-14 23:19:31 -0800

    hehe

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$('a').click(function(event){
-    event.preventDefault(); 
-});
+// $('a').click(function(event){
+//     event.preventDefault(); 
+// });
+
+var button = document.getElementById("button");
+var draft = document.getElementById("draft");
+var notdraft = document.getElementById("notdraft");
+
+
+button.onclick = function() {
+    if (Math.random() <= 0.2) {
+        draft.style.display = 'block'
+        notdraft.style.display = 'none'
+    } else {
+        notdraft.style.display = 'block'
+        draft.style.display = 'none'
+    }
+};
